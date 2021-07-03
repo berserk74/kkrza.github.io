1b823027e0aeb27f1c4f5f1e7d6236de1d21cb6b
CakesZero <buffotgame@gmail.com>
2021-07-04 03:20:45 +0500

    nether update!!!

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
-let search = window.location.search
+// let search = window.location.search
 
-let text = document.getElementById('text').innerHTML = search
+// let text = document.getElementById('text').innerHTML = search
+
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  AdaptivityProvider,
+  ConfigProvider,
+  useAdaptivity,
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  ViewWidth,
+  View,
+  Panel,
+  PanelHeader,
+  Header,
+  Group,
+  SimpleCell
+} from "@vkontakte/vkui";
+import "@vkontakte/vkui/dist/vkui.css";
+
+const Example = () => {
+  const { viewWidth } = useAdaptivity();
+
+  return (
+    <AppRoot>
+      <SplitLayout header={<PanelHeader separator={false} />}>
+        <SplitCol spaced={viewWidth > ViewWidth.MOBILE}>
+          <View activePanel="main">
+            <Panel id="main">
+              <PanelHeader>VKUI</PanelHeader>
+              <Group header={<Header mode="secondary">Items</Header>}>
+                <SimpleCell>Hello</SimpleCell>
+                <SimpleCell>World</SimpleCell>
+              </Group>
+            </Panel>
+          </View>
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
+  );
+};
+
+ReactDOM.render(
+  <ConfigProvider>
+    <AdaptivityProvider>
+      <Example />
+    </AdaptivityProvider>
+  </ConfigProvider>,
+  document.getElementById("root")
+);
