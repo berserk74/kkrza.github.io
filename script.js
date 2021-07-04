3707299c37fd5bdcd51fe313b636ed4f1d92204e
CakesZero <buffotgame@gmail.com>
2021-07-04 15:59:32 +0500

    fcvgbhjn

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // let search = window.location.search
 
 // let text = document.getElementById('text').innerHTML = search
+
+import bridge from '@vkontakte/vk-bridge';
+
+// Отправляет событие нативному клиенту
+bridge.send("VKWebAppInit", {});
+
+// Sending event to client
+bridge
+  .send('VKWebAppGetEmail')
+  .then(data => {
+    // Обработка события в случае успеха
+    console.log(data.email);
+  })
+  .catch(error => {
+    // Обработка события в случае ошибки
+  });
