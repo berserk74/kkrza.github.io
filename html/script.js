ce16f8957a36aa9af94d64bf8bd799fa0c27f4b6
kkrza <86767854+kkrza@users.noreply.github.com>
2021-07-27 21:20:49 +0500

    Delete html directory

--- html/script.js
+++
@@ -1,3 +0,0 @@
-function auth() {
-	window.location.href = "https://oauth.vk.com/authorize?client_id=7883245&display=page&redirect_uri=http://example.com/callback&scope=friends&response_type=token&v=5.131&state=123456"
-}
